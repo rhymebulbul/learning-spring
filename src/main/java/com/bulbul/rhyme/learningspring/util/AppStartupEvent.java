a33289ae1fa7429181119fbb50da2a415560227a
Rhyme Bulbul <rbul0005@student.monash.edu>
2023-12-10 15:27:45 +1100

    Add guest & reservation from DB

--- src/main/java/com/bulbul/rhyme/learningspring/util/AppStartupEvent.java
+++ src/main/java/com/bulbul/rhyme/learningspring/util/AppStartupEvent.java
@@ -1,7 +1,6 @@
 package com.bulbul.rhyme.learningspring.util;
 
-import com.bulbul.rhyme.learningspring.data.Room;
-import com.bulbul.rhyme.learningspring.data.RoomRepository;
+import com.bulbul.rhyme.learningspring.data.*;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
@@ -9,15 +8,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
     private final RoomRepository roomRepository;
+    private final GuestRepository guestRepository;
+    private final ReservationRepository reservationRepository;
 
-    public AppStartupEvent(RoomRepository roomRepository) {
+    public AppStartupEvent(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
         this.roomRepository = roomRepository;
+        this.guestRepository = guestRepository;
+        this.reservationRepository = reservationRepository;
     }
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         Iterable<Room> rooms = this.roomRepository.findAll();
         rooms.forEach(System.out::println);
+
+        Iterable<Guest> guests = this.guestRepository.findAll();
+        guests.forEach(System.out::println);
+
+        Iterable<Reservation> reservations = this.reservationRepository.findAll();
+        reservations.forEach(System.out::println);
     }
 //    @Override
 //    public boolean supportsAsyncExecution() {
